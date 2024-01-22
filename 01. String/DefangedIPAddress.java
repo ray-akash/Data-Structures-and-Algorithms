31e87a0d6d1337f70b8c9f1fdf6b7ebafe630cbf
Akash Ray <79934815+ray-akash@users.noreply.github.com>
2024-01-22 23:10:46 +0530

    Create DefangedIPAddress.java

---
+++ 01. String/DefangedIPAddress.java
@@ -0,0 +1,11 @@
+/*
+*    1108. Defanging an IP Address
+*    Given a valid (IPv4) IP address, return a defanged version of that IP address.
+*    A defanged IP address replaces every period "." with "[.]".
+*/
+
+class Solution {
+    public String defangIPaddr(String address) {
+        return address.replace(".", "[.]");
+    }
+}
