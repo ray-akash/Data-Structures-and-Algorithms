ca8317b05ca16fb1b9a2b08ebced3ddb609c55c6
Akash Ray <79934815+ray-akash@users.noreply.github.com>
2024-01-29 17:55:40 +0530

    Update NumbersWEvenNoOfDigits.java

--- 03. Linear Search/NumbersWEvenNoOfDigits.java
+++ 03. Linear Search/NumbersWEvenNoOfDigits.java
@@ -1,3 +1,8 @@
+/*
+*    1295. Find numbers with even number of digits
+*    Given an array nums of integers, return how many of them contain an even number of digits.
+*/
+
 class Solution {
     public int findNumbers(int[] nums) {
         var counter = 0;
