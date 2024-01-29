bd59f6a26a14a1cc851a1791f2ce3f581484a291
Akash Ray <79934815+ray-akash@users.noreply.github.com>
2024-01-29 18:05:22 +0530

    Create SmallestLetterGreaterThanTarget.java

---
+++ 04. Binary Search/SmallestLetterGreaterThanTarget.java
@@ -0,0 +1,28 @@
+/*
+*  744. Find Smallest Letter Greater Than Target
+*  You are given an array of characters letters that is sorted in non-decreasing order, 
+*  and a character target. There are at least two different characters in letters.
+*
+*  Return the smallest character in letters that is lexicographically greater than target. 
+*  If such a character does not exist, return the first character in letters.
+*/
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        return ceilValue(letters, target);
+    }
+
+    public static char ceilValue(char[] letters, char target){
+        int start = 0;
+        int end = letters.length-1;
+
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < letters[mid])
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return letters[start%letters.length];
+    }
+}
