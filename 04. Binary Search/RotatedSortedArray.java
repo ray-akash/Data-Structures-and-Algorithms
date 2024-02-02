90a28fd87f58aa70e2fcf920753886aaf212ecc7
Akash Ray <79934815+ray-akash@users.noreply.github.com>
2024-02-02 08:19:25 +0530

    Update RotatedSortedArray.java

--- 04. Binary Search/RotatedSortedArray.java
+++ 04. Binary Search/RotatedSortedArray.java
@@ -1,49 +1,57 @@
 // 33.  https://leetcode.com/problems/search-in-rotated-sorted-array/
 
 class Solution {
-    public int search(int[] nums, int target) {
-        int pivot = findPivot(nums);
-        if(pivot == -1)
+    public static int search(int[] nums, int target) {
+        // 1. Find pivot
+        int pivotIndex = findPivot(nums);
+
+        // If the array is not rotated, do binary search over whole array
+        if(pivotIndex == -1) {
             return binarySearch(nums, target, 0, nums.length-1);
-        
-        if(nums[pivot] == target)
-            return pivot;
+        }
 
-        if(target >= nums[0])
-            return binarySearch(nums, target, 0, pivot-1);
+        // If pivot is only the target 
+        if(nums[pivotIndex] == target)
+            return pivotIndex;
 
-        return binarySearch(nums, target, pivot+1, nums.length-1);
+        // 2. Search for target in both halves
+        if(nums[0] <= target)
+            return binarySearch(nums, target, 0, pivotIndex-1);
+        else
+            return binarySearch(nums, target, pivotIndex+1, nums.length-1);
     }
 
-    public int findPivot(int[] arr){
+    public static int findPivot(int[] arr){
         int start = 0;
         int end = arr.length-1;
 
         while(start <= end){
-            int mid = start + (end-start)/2;
-            
-            if(mid < end && arr[mid] > arr[mid+1])
+            int mid = start+(end-start)/2;
+            if(mid+1 <= end && arr[mid] > arr[mid+1])
                 return mid;
-            if(mid > start && arr[mid] < arr[mid-1])
+            if(mid-1 >= start && arr[mid] < arr[mid-1])
                 return mid-1;
-            if(arr[mid] <= arr[start])
+            if(arr[start] >= arr[mid])
                 end = mid-1;
-            else
+            if(arr[start] < arr[mid])
                 start = mid+1;
         }
+        // In case the array is not rotated, -1 will be returned
         return -1;
     }
 
-    public int binarySearch(int [] arr, int target, int start, int end){
+    public static int binarySearch(int[] arr, int target, int start, int end){
         while(start <= end){
-            int mid = start + (end-start)/2;
+            int mid = start+(end-start)/2;
             if(arr[mid] < target)
                 start = mid+1;
             else if(arr[mid] > target)
                 end = mid-1;
             else
+                // target found
                 return mid;
         }
+        // target not found
         return -1;
     }
 }
