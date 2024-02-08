27aacb32a3ca9d69b3e5452621d13629cdf0d8ed
Akash Ray <79934815+ray-akash@users.noreply.github.com>
2024-02-08 19:11:19 +0530

    Update InsertionSort.java

--- 05. Sorting/InsertionSort.java
+++ 05. Sorting/InsertionSort.java
@@ -9,9 +9,9 @@ import java.util.Arrays;
 public class InsertionSort {
     public static void main(String[] args){
         int[] arr = {3, 7, 3, 4, 4, 6, 7, -7, 7, 0, 0, 1, 4, 3, 6, 5, -5, 4, 2,};
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr));    // [3, 7, 3, 4, 4, 6, 7, -7, 7, 0, 0, 1, 4, 3, 6, 5, -5, 4, 2]
         insertionSort(arr);
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr));    // [-7, -5, 0, 0, 1, 2, 3, 3, 3, 4, 4, 4, 4, 5, 6, 6, 7, 7, 7]
     }
 
     static void insertionSort(int[] arr){
