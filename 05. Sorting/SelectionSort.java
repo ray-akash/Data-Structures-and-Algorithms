11250049263f070e3e0a5a85ccdee45fa19c841b
Akash Ray <79934815+ray-akash@users.noreply.github.com>
2024-02-08 19:10:27 +0530

    Update SelectionSort.java

--- 05. Sorting/SelectionSort.java
+++ 05. Sorting/SelectionSort.java
@@ -1,17 +1,17 @@
 /*
-*  Time Complexity: O(n^2)
-*  Space Complexity: O(1)
-*  Unstable
+*    Time Complexity: O(n^2)
+*    Space Complexity: O(1)
+*    Unstable
 */
 package Sorting;
 
 import java.util.Arrays;
 public class SelectionSort {
     public static void main(String[] args){
-        int[] arr = {3, 3, 4, 4, 6, 7, -7, 7, 0, 0, 1, 4, 3, 6, 5, 5, -5, 4, 2};
-        System.out.println(Arrays.toString(arr));
+        int[] arr = {3, 7, 3, 4, 4, 6, 7, -7, 7, 0, 0, 1, 4, 3, 6, 5, -5, 4, 2,};
+        System.out.println(Arrays.toString(arr));    // [3, 7, 3, 4, 4, 6, 7, -7, 7, 0, 0, 1, 4, 3, 6, 5, -5, 4, 2]
         selectionSort(arr);
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr));    // [-7, -5, 0, 0, 1, 2, 3, 3, 3, 4, 4, 4, 4, 5, 6, 6, 7, 7, 7]
     }
 
     static void selectionSort(int[] arr){
@@ -19,7 +19,7 @@ public class SelectionSort {
             // Get the index of max element in range [0,i]
             int index = getMaxIndex(arr, i);
 
-            // Swap and place max element at correct position
+            // Place max element at correct position
             int temp = arr[i];
             arr[i] = arr[index];
             arr[index] = temp;
